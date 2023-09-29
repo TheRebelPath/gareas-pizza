8512f6ad26b9c3fa7f925605bc306ba33e64a9ab
Diego G <d.g.g.87@hotmail.com>
2023-09-29 01:01:55 -0600

    added menu constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,4 +28,79 @@ const navLinks = [
   },
 ];
 
-export {navLinks};
+const clasicas = [
+  {
+    title: 'Clasica',
+    price: 169,
+    ingredientes:
+      'Combinacion de quesos mozarella y gouda, pepperoni y un leve toque de especias italianas',
+  },
+  {
+    title: 'Hawaiana',
+    price: 189,
+    ingredientes:
+      'Combinacion de quesos mozarella y gouda, jamon, pina y un leve toque de especias italianas',
+  },
+  {
+    title: 'Margarita',
+    price: 199,
+    ingredientes:
+      'Combinacion de quesos mozarella y gouda, jamon, pina y un leve toque de especias italianas',
+  },
+];
+
+const carnivoras = [
+  {
+    title: 'Atascada',
+    price: 249,
+    ingredientes: 'Combinacion de quesos mozarella y gouda, jamon de pierna, salami, pepperoni, chorizo, tocino ahumado, salchicha y un leve toque de especias italianas'
+  }
+  {
+    title: 'PepeChamps',
+    price: 189,
+    ingredientes: 'Combinacion de quesos mozarella y gouda, pepperoni, chorizo, champinon y un leve toque de especias italianas'
+  },
+  {
+    title: 'Mexicana'
+    price: '219'
+    ingredientes: 'Combinacion de quesos mozarella y gouda, chorizo, jalapeno, cebolla, pimientio morron y un leve toque de especias italianas'
+  },
+]
+
+const vegetarianas = [
+  {
+    title: 'Vegetariana',
+    price: '199',
+    ingredientes: 'Combinacion de quesos mozarella y gouda, pimiento morron, champinones, aceitunas y un leve toque de especias italianas',
+  },
+  {
+    title: 'Del Huerto',
+    price: '???',
+    ingredientes: 'Combinacion de quesos mozarella y gouda, champinones, jitomate cherry, albahaca y un leve toque de especias italianas',
+  },
+  {
+    title: 'Herbivora',
+    price: '229',
+    ingredientes: 'Combinacion de quesos mozarella y gouda, champinones, jitomate cherry, pimiento morron, aceituna negra, cebolla, albahaca y un leve toque de especias italianas',
+  },
+]
+
+const especialidades = [
+  {
+    title: 'Alemana',
+    price: 209,
+    ingredientes: 'Combinacion de quesos mozarella y gouda, salchicha rostizada al ajo, hongos portobello y un leve toque de especias italianas',
+  },
+  {
+    title: 'Cabra',
+    price: 229,
+    ingredientes: 'Combinacion de quesos mozarella y gouda, jitomate cherry, queso de cabra, albahaca y un leve toque de especias italianas',
+  },
+  {
+    title: 'Espanola',
+    price: 249,
+    ingredientes: 'Combinacion de quesos mozarella y gouda, jamon serrano, queso de cabra, chimichurri y un leve toque de especias italianas',
+  },
+]
+
+export {navLinks, clasicas, carnivoras, vegetarianas, especialidades};
